c30b62be5f05b5362a4bf34bf45391199679b628
Mark Yin <xtm.yin@live.com>
2015-06-23 13:54:08 +1200

    new files

---
+++ constellation.js
@@ -0,0 +1,5 @@
+var constellation [
+{
+	"name":"Southern Cross",
+	"info": "The Southern Cross is a pattern of stars (constellation) in the Milky Way galaxy. The stars are visible in the southern hemisphere sky. It was named by 16th-cenury European explorers, who recognised the shape of a cross. Four bright stars form the outline, with another fainter star just below the crossbar."
+}]
---
+++ stardata.js
@@ -0,0 +1,8 @@
+var starPoint [
+{
+	"name": "Southern Cross",
+	"points": [{"x": 0, "y": 7},{"x": 3, "y": 4},{"x": 0, "y": 0}{"x": -3, "y": 4}]
+
+}
+]
+
